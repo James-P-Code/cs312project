7c49e7c57dcb511026ea3b2268b892bddfefee85
declan <decjmclaughlin@gmail.com>
2025-04-09 16:50:10 -0600

    Updated page routing to start on /color instead of the blank index.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { AboutComponent } from './about/about.component';
 
 
 export const routes: Routes = [
+    { 
+        path: '', 
+        redirectTo: '/color', // Redirect root path
+        pathMatch: 'full' }, 
     {
         path: 'color',
         component: ColorComponent,
