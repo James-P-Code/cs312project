42397a20bc6946d168d508ed7e77780e0cc0b35d
James Pochas <c837426514@colostate.edu>
2025-04-23 03:09:56 -0600

    Initial API/Database setup

---
+++ src/app/api/database.ts
@@ -0,0 +1,15 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+
+export class Database {
+    readonly apiURL: string = "https://cs.colostate.edu:4444/~c837426514/api/api.php";
+
+    constructor(private http: HttpClient) {}
+
+    public getColors() {
+        const params = new HttpParams().set('param', 'colors');
+        return this.http.get<{id: number, name: string, hex_value : string}[]>(this.apiURL, {params});
+    }
+}
