2e10be63216c0f13e2a5b64acccd31b5c48a3869
Rose Ordway <ordway99@gmail.com>
2025-04-25 13:26:13 -0600

    Color-Coordinate List Working

--- src/app/api/database.ts
+++ src/app/api/database.ts
@@ -2,21 +2,22 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
-
+@Injectable({ providedIn: 'root' })
 export class Database {
-    readonly apiURL: string = "https://cs.colostate.edu:4444/~c837426514/api/api.php";
+  readonly apiURL: string =
+    'https://cs.colostate.edu:4444/~c837426514/api/api.php';
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    public getRequest<T>(params: HttpParams) {
-        return this.http.get<T>(this.apiURL, { params });
-    }
+  public getRequest<T>(params: HttpParams) {
+    return this.http.get<T>(this.apiURL, { params });
+  }
 
-    public postRequest(action: string, postParams: Map<string, string>) {
-        const params = {action, ...Object.fromEntries(postParams)};
+  public postRequest(action: string, postParams: Map<string, string>) {
+    const params = { action, ...Object.fromEntries(postParams) };
 
-        return this.http.post(this.apiURL, params, { observe: 'response' })
-                        .pipe(map(response => response.status));  
-    }
+    return this.http
+      .post(this.apiURL, params, { observe: 'response' })
+      .pipe(map((response) => response.status));
+  }
 }
