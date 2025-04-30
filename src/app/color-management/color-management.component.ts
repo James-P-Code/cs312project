ca811fc2f135149d69dbae0ee483b338429f6c25
Grace Terrell <169406593+grace-evelyn@users.noreply.github.com>
2025-04-30 12:37:53 -0600

    updated color management ts

--- src/app/color-management/color-management.component.ts
+++ src/app/color-management/color-management.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AddColorComponent } from "./add-color/add-color.component";
+import { EditColorComponent } from "./edit-color/edit-color.component";
 
 @Component({
   selector: 'app-color-management',
-  imports: [AddColorComponent],
+  imports: [AddColorComponent, EditColorComponent],
   templateUrl: './color-management.component.html',
   styleUrl: './color-management.component.css'
 })
