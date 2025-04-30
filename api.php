f6ec0000f47ebe4ce819c4b623fbd8fde8c158dd
James Pochas <c837426514@colostate.edu>
2025-04-29 23:31:45 -0600

    Update API to support edt and delete

--- api.php
+++ api.php
@@ -3,8 +3,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Allow-Headers: Content-Type");
 include 'database_info.php';
-const QUERY_SUCCESS_CODE = 201;
-const QUERY_ERROR_CODE = 409;
+define('QUERY_SUCCESS_CODE', 201);
+define('QUERY_ERROR_CODE', 400);
 
 $servername = "faure";
 $username = $net_id;
@@ -33,10 +33,22 @@ function handlePostRequest(mysqli $connection) {
     $post_data = json_decode(file_get_contents('php://input'), true);
 
     switch ($post_data['action']) {
-        case "add": addColor($connection, $post_data);
+        case "add": addColor($connection, $post_data); break;
+        case "edit": editColor($connection, $post_data); break;
+        case "delete": deleteColor($connection, $post_data); break;
     }
 }
 
+/* 
+    Post request parameters getting the colors from the database:
+    param = colors (string)
+    Returns JSON in the following format:
+    {
+        "id": "1",
+        "name": "red",
+        "hex_value": "#FF0000"
+    }  
+*/
 function getColors(mysqli $connection){
     $sql = "SELECT * FROM colors ORDER BY id";
     $query_result = $connection->query($sql);
@@ -50,6 +62,14 @@ function getColors(mysqli $connection){
     echo json_encode($database_data);
 }
 
+/* 
+    Post request parameters getting the amount of colors in database:
+    param = count (string)
+    Retruns JSON in the following format:
+    {
+        "count": 51
+    }  
+*/
 function getColorCount(mysqli $connection) {
     $sql = "SELECT COUNT(*) AS count FROM colors";
     $query_result = $connection->query($sql);
@@ -59,28 +79,73 @@ function getColorCount(mysqli $connection) {
     echo json_encode(['count' => (int)$color_count['count']]);
 }
 
+/* Post request parameters for adding a color:
+    action = add (string)
+    colorName = name of color to add (string)
+    colorValue = hex value of color to add (string)  */
 function addColor(mysqli $connection, $post_data) {
-    $sql = $connection->prepare("INSERT INTO colors (name, hex_value) VALUES (?, ?)");
-    $name = $post_data['colorName'];
-    $hex_value = $post_data['colorValue'];
-
-    $sql->bind_param("ss", $name, $hex_value);
-
+    $sql = $connection->prepare("INSERT INTO colors (name, hex_value) VALUES (?, ?)");;
+    $sql->bind_param("ss", $post_data['colorName'], $post_data['colorValue']);
     $sql->execute();
-    $responseCode = 0;
 
+    sendPostResponse($sql, "Color added successfully");
+}
+
+/* Post request parameters for editing a color:
+    action = edit (string)
+    id = database id number of color to edit (number/integer)
+    colorName = name of color (string)
+    colorValue = hex value of color (string) */
+function editColor(mysqli $connection, $post_data) {
+    $sql = $connection->prepare("UPDATE colors SET name = ?, hex_value = ? WHERE id = ?");
+    $sql->bind_param("ssi", $post_data["colorName"], $post_data["colorValue"], $post_data["id"]);
+    $sql->execute();
+
+    sendPostResponse($sql, "Color edited successfully");
+}
+
+/* Post request parameters for deleting a color:
+    action = delete (string)
+    id = database id number of color to delete (number/integer)
+    Note, if there are <= 2 colors in the database the deletion will fail */
+function deleteColor(mysqli $connection, $post_data) {
+    if (isOkToDelete($connection)) {
+        $sql = $connection->prepare("DELETE FROM colors WHERE id = ?");
+        $sql->bind_param("i", $post_data["id"]);
+        $sql->execute();
+
+        sendPostResponse($sql, "Color deleted successfully");
+    } else {
+        http_response_code(QUERY_ERROR_CODE);
+        echo json_encode([
+            'responseCode' => QUERY_ERROR_CODE,
+            'message' => 'Database must contain at least 2 colors'
+        ]);
+    }
+}
+
+function isOkToDelete(mysqli $connection) {
+    $minimumColors = 2;
+    $sql = "SELECT COUNT(*) AS count FROM colors";
+    $query_result = $connection->query($sql);
+    $color_count = $query_result->fetch_assoc();
+
+    return (int)$color_count['count'] > $minimumColors;
+}
+
+function sendPostResponse($sql, $success_message) {
     header('Content-Type: application/json');
     if ($sql->errno) {
-        http_response_code(400);
+        http_response_code(QUERY_ERROR_CODE);
         echo json_encode([
             'responseCode' => $sql->errno,
             'message' => 'Database error: ' . $sql->error
         ]);
     } else {
-        http_response_code(201);
+        http_response_code(QUERY_SUCCESS_CODE);
         echo json_encode([
-            'responseCode' => 201,
-            'message' => 'Color added successfully'
+            'responseCode' => QUERY_SUCCESS_CODE,
+            'message' => $success_message
         ]);
     }
 }
