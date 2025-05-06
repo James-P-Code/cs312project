0a582ff0b9e2b73320179b71d1de60564e22419f
James Pochas <c837426514@colostate.edu>
2025-05-06 00:39:14 -0600

    Add polish to "Add Color" form

--- src/app/color-management/add-color/add-color.component.ts
+++ src/app/color-management/add-color/add-color.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { Database } from '../../api/database';
 import { HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-add-color',
   imports: [ReactiveFormsModule],
   templateUrl: './add-color.component.html',
-  styleUrl: './add-color.component.css'
+  styleUrl: '../color-management.component.css'
 })
 export class AddColorComponent {
 
   public addColorForm!: FormGroup;
   public addColorSuccess: boolean = false;
-  public addColorFailure: boolean = false;
+  public isSubmitted: boolean = false;
+  public isDuplicateColorName: boolean = false;
+  public isDuplicateHexValue: boolean = false;
 
   public constructor(private database: Database) {
     this.initializeAddColorForm();
@@ -27,43 +30,46 @@ export class AddColorComponent {
       colorValue: new FormControl('#e26daa', [
         Validators.required // grace - validators required
       ])});
+
+    this.addColorForm.markAsPristine();
+    this.addColorSuccess = false;
+    this.isSubmitted = false;
+    this.isDuplicateColorName = false;
+    this.isDuplicateHexValue = false;
   }
 
-  get addColorFormColorName() {
-    return this.addColorForm.get('colorName');
+  public get colorName() {
+    return this.addColorForm.get('colorName') as FormControl;
   }
 
-  get addColorFormColorValue() {
-    return this.addColorForm.get('colorValue');
+  public reinitializeFormIfNeeded(): void {
+    if (this.addColorForm.dirty && this.isSubmitted) {
+      this.initializeAddColorForm();
+    }
   }
 
   public onAddColorSubmit(): void { // grace - void
-    this.addColorSuccess = false; // grace - added these next few lines
-    this.addColorFailure = false;
+    this.isSubmitted = true;
 
     if (this.addColorForm.invalid) return;
-
-    const successCode = 201;
 
     let postParams = new Map<string, any>([
         ["colorName", String(this.addColorForm.value.colorName)],
         ["colorValue", String(this.addColorForm.value.colorValue)]
     ]);
 
+    const successCode = 201;
+
     this.database.postRequest("add", postParams).subscribe({ // grace - changed this section
       next: responseCode => {
         if (responseCode == successCode) {
           this.addColorSuccess = true;
-          this.addColorForm.reset({ colorValue: '#e26daa' });
-        } else {
-          this.addColorFailure = true;
         }
-        // console.log("Response code: " + responseCode);
-        // responseCode == successCode ? this.addColorSuccess = true : this.addColorFailure = true;
       },
-      error: (errorResponse: HttpErrorResponse) => {
-        console.log("Error: " + errorResponse.message);
-        this.addColorFailure = true;
+      error: (response: HttpErrorResponse) => {
+        console.log("Error: " + response.message);
+        this.isDuplicateColorName = response.error.message.includes("unique_name");
+        this.isDuplicateHexValue = response.error.message.includes("unique_hex_value");
       }
     });
   }
